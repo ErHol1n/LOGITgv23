cc1996d1f44af360eaeb3d2338cdc89ba3709b34
Laptop <erik.holim@gmail.com>
2024-01-10 16:53:45 +0200

    Work

--- Ülesanded/Ülesane_3.py
+++ Ülesanded/Ülesane_3.py
@@ -1,8 +1,7 @@
-from random import *
 #Pinginaabrid
 #'A', 'B'
 n1=input('Esimene nimi ')
-n2=input('Teine nimi')
+n2=input('Teine nimi ')
 if n1.upper()=='A' and n2.upper()=='B'or n1.upper()=='B' and n2.upper()=='A':
     print('Pinginaabrid')
 else:

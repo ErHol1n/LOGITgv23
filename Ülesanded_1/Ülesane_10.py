ff25740e627c57e6c12f148adc94ef0ab04fe08a
Laptop <erik.holim@gmail.com>
2024-01-22 17:23:21 +0200

    Work

--- Ülesanded_1/Ülesane_10.py
+++ Ülesanded_1/Ülesane_10.py
@@ -14,7 +14,9 @@ if mt in ['+', '-', '*', '/']:
             answer = a / b
         else:
             print('Ошибка: деление на ноль!')
-            exit()
     print('Ответ: ', answer)
 else:
     print('Читайте внимательнее!!!')
+
+# Diagrams  https://lucid.app/lucidchart/03ef5c02-2668-4f4d-8444-fd16ca66255f/edit?viewport_loc=35%2C-196%2C3796%2C1792%2C0_0&invitationId=inv_30dd24dd-abdb-42d4-b4dc-4de7b3637356
+
--- Ülesanded_1/Ülesane_9.py
+++ Ülesanded_1/Ülesane_9.py
@@ -7,3 +7,5 @@ if a==b==c==d:
 else:
     print('Одна из сторон была указана непавильно это значит что квадрат не истинный')
 
+# Diagrams  https://lucid.app/lucidchart/03ef5c02-2668-4f4d-8444-fd16ca66255f/edit?viewport_loc=35%2C-196%2C3796%2C1792%2C0_0&invitationId=inv_30dd24dd-abdb-42d4-b4dc-4de7b3637356
+
